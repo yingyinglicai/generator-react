f4726c40b052c5acd2c52bc9e3d76d32bbf90ce4
mmcai <mmcai1013@gmail.com>
2016-11-09 23:48:15 +0800

    添加了react-router demo 代码和fastclick插件

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+/*eslint no-console:0 */
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, browserHistory } from 'react-router'
+import 'react-fastclick';
 
 import 'normalize.css';
 import './public/style/iconfont.css';
@@ -10,4 +13,21 @@ import First from './routes/page1/index';
 
 console.log("index");
 
-ReactDOM.render(<First />, document.getElementById('app'));
+
+//定义路由对象
+const rootRoute = {
+  childRoutes: [
+    {
+      path: '/',
+      component: First
+    }
+  ]
+};
+
+ReactDOM.render((
+  <Router
+    history={browserHistory}
+    routes={rootRoute}
+  />
+), document.getElementById('app'));
+
